e312b2be6daa78274491bbbbdb920555a9d9d44e
Will Harris <bharris010486@gmail.com>
2026-04-07 19:42:18 -0400

    Inline stylesheet assets for performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://parrishtreeremoval.com",
   output: "static",
+  build: {
+    inlineStylesheets: "always",
+  },
   devToolbar: {
     enabled: false,
   },
