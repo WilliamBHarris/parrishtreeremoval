a85e1005cd9f9285b8a0cb94122760c4ed394b02
Will Harris <bharris010486@gmail.com>
2026-04-07 20:22:09 -0400

    Inline section CSS and fix remaining contrast

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://parrishtreeremoval.com",
   output: "static",
-  vite: {
-    build: {
-      cssCodeSplit: false,
-    },
+  build: {
+    inlineStylesheets: "always",
   },
   devToolbar: {
     enabled: false,
